5b7c87d4fb51ff433016ca26f13573f3edb51a74
程意絜 <chengyijie@chengyijiedeMacBook-Pro.local>
2022-12-25 22:47:22 +0800

    test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import db from './backend/db.js'
 import routes from './backend/routes/index.js';
 import dotenv from "dotenv-defaults";
 dotenv.config();
+import wakeUpDyno from "./backend/route/wakeUpDyno.js";
  
 const app = express();
 const port = process.env.PORT || 4000;
@@ -13,7 +14,9 @@ app.use('/', routes);
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
-app.listen(port, () =>
+app.listen(port, () => {
+    const DYNO_URL = "https://my.app_A_heroku.com/"
+    wakeUpDyno(DYNO_URL)
     console.log(`Example app listening on port ${port}!`),
-);
+});
 db.connect();
